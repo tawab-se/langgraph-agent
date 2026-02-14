dfa11f1593ecfe6fa7a3ad1ee5ed33798688c512
tawab-se <tawabsarhadi111@gmail.com>
2026-02-14 16:55:44 +0500

    Switched model to Gemini 2.0 due to quota limit exceed

--- src/llm/gemini.client.ts
+++ src/llm/gemini.client.ts
@@ -15,7 +15,7 @@ export class GeminiClient {
 
     this.model = new ChatGoogleGenerativeAI({
       apiKey,
-      model: 'gemini-3-flash-preview',
+      model: 'gemini-2.0-flash',
       temperature: 0.1,
     });
   }
